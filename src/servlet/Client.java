c6042fd3efd4239d22b5e5599ed950bced3f7c78
Ayushi Singh <ayushisi@usc.edu>
2019-12-02 01:29:22 -0800

    Update Client.java

--- src/servlet/Client.java
+++ src/servlet/Client.java
@@ -1,131 +1,222 @@
-package servlet;
-
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Random;
 import java.util.Scanner;
 import java.util.Vector;
 
 import message.Message;
 
-public class Client {
-	
-	private ObjectOutputStream oos;
-	private ObjectInputStream ois;
 
+public class Server{
+	public static Scanner in;
+	//private static Vector<MusicRoom> games;
+	private Vector<ServerThread> serverThreadsS;
+	private Vector<MusicRoom> musicroom;
 	
-	public Client(String host, int p) throws ClassNotFoundException {
-		
-		
+	public Server (){
+		System.out.print("Trying to connect to server...");
+		System.out.print("hi");
+		System.out.print("hi");
+		musicroom = new Vector<MusicRoom>();
 		try {
-			System.out.println("Trying to connect to server...Connected!");
-			Socket s = new Socket(host, p);
-			this.oos = new ObjectOutputStream(s.getOutputStream());
-			this.ois = new ObjectInputStream(s.getInputStream());
-//			this.br = new BufferedReader(new InputStreamReader(s.getInputStream()));
-//			this.ps = new PrintWriter(s.getOutputStream());
-			
-
+			ServerSocket ss = new ServerSocket(6789);
+			System.out.print("Connected!\n");
+			serverThreadsS = new Vector<ServerThread>();
+			while(true) {
+				Socket s = ss.accept(); // blocking
+				System.out.println("Connection from: " + s.getInetAddress());
+				ServerThread st = new ServerThread(s, this);
+				serverThreadsS.add(st);
+			}
 		} catch (IOException ioe) {
-			System.out.println("ioe in client constructor: " + ioe.getMessage());
+			System.out.println("Unable to connect to server " + "localhost" + " on port 6789");
+			ioe.printStackTrace();
+		}
+	}
+	public void addSong(Vector<String>input, ServerThread st) {
+		String output = "";
+		String songName = input.get(0);
+		String roomCode = input.get(1);
+		String username = input.get(2);
+		MusicRoom r = null;
+		System.out.println("addSong");
+		
+		for(int i = 0; i < musicroom.size(); i++) {
+			if(musicroom.get(i).getRoomName().equals(roomCode)) {
+				r = musicroom.get(i);
+			}
+		}
+		if(null == r) { //room does not exist
+			r = new MusicRoom(roomCode);
+			musicroom.add(r);
+			if(!r.findUser(username)) { //if user is not there
+				r.addUser(username, st);
+			} 
+			r.addSong(songName);
+		} 
+		else { //room is found
+			if(!r.findUser(username)) { //if user is not there
+				r.addUser(username, st);
+			}
+			r.addSong(songName);
+		}
+		Vector<String> allSongs = r.getAllSongs();
+		for(int i = 0; i < allSongs.size(); i++) {
+			output += allSongs.get(i) + ",";
+		}
+		System.out.println("ADD SONG: " + output);
+		Vector<String> outputSongs = new Vector<String>();
+		outputSongs.add(output);
+		Vector<ServerThread> serverT = new Vector<ServerThread>();
+		serverT = r.allSt();
+		System.out.println("NEW SIZE IS " + serverT.size());
+		for(int i = 0; i < serverT.size(); i++) {
+			Message m = new Message("output", outputSongs);
+			st.sendMessage(m);
 		}
 	}
 	
 	
-	
-	public String addSong(String songName, String roomCode, String username) throws ClassNotFoundException, IOException {
-		
-		Vector<String> input = new Vector<String>();
-		input.add(songName);
-		input.add(roomCode);
-		input.add(username);
-		
-		Message m = new Message("ADD_SONG", input);
-		
-		this.sendMessage(m);
-		
-		Message mr = (Message)ois.readObject();
-		
-		return mr.getInput().get(0);
-		
-		
-		
+	public void removeSong(Vector<String> input, ServerThread st) {
+		String output = "";
+		String roomCode = input.get(0);
+		String username = input.get(1);
+		String songName = input.get(2);
+		MusicRoom r = null;
+		System.out.println("removeSong");
+		System.out.println(roomCode);
+		System.out.println(username);
+		System.out.println(songName);
+		for(int i = 0; i < musicroom.size(); i++) {
+			if(musicroom.get(i).getRoomName().equals(roomCode)) {
+				r = musicroom.get(i);
+			}
+		}
+		System.out.println("remove song size:" + musicroom.size());
+		r = musicroom.get(0);
+		System.out.println("GOING TO DeLETE SONGFROM MUSICROOM");
+		r.removeSong(songName);
+		Vector<String> allSongs = r.getAllSongs();
+		// append it shorter
+		for(int i = 0; i < allSongs.size(); i++) {
+			output += allSongs.get(i) + ",";
+		}
+		System.out.println("music room OUTPUT STRING: " + output);
+		Vector<String> outputSongs = new Vector<String>();
+		outputSongs.add(output);
+		Vector<ServerThread> serverT = new Vector<ServerThread>();
+		serverT = r.allSt();
+		System.out.println("music room NEW SIZE IS " + serverT.size());
+		for(int i = 0; i < serverT.size(); i++) {
+			Message m = new Message("output", outputSongs);
+			st.sendMessage(m);
+		}
 	}
 	
-	public void addST(String username) {
-		Vector<String> input = new Vector<String>();
-		input.add(username);
-		Message m = new Message("ADD_ST", input);
-		
-		this.sendMessage(m);
-		
+	public void addST(Vector<String>input, ServerThread st) {
+		String username = input.get(0);
+		MusicRoom r = musicroom.get(0); //JUST GOT THE 1ST ROOM, WOULD NEED CHANGING IF DONE A BETTER WAY
+		r.addUser(username, st);
+		Vector<ServerThread> serverT = new Vector<ServerThread>();
+		serverT = r.allSt();
+		System.out.println("NEW SIZE IS " + serverT.size());
 		return;
 	}
 	
-	
-	public String upVoteU(String song) throws ClassNotFoundException, IOException {
-		Vector<String> input = new Vector<String>();
-		input.add(song);
-		System.out.println("IN CLIENT BEFORE:" + song);
-		Message m = new Message("UP_VOTE", input);
-		
-		this.sendMessage(m);	
-		
-		Message mr = (Message)ois.readObject();
-		
-		System.out.println("IN client VOTE " + mr.getInput().get(0));
-			
-		return mr.getInput().get(0);
-	}
-
-	public void sendMessage(Message cm) {
-
-		try {
-			oos.writeObject(cm);
-			oos.flush();
-		} catch (IOException ioe) {
-			System.out.println("ioe: " + ioe.getMessage());
+	public void upVote(Vector<String>input, ServerThread st) {
+		String output = "";
+		String songName = input.get(0);
+		MusicRoom r = musicroom.get(0);
+		r.upVote(songName);
+		System.out.println("upVote in SERVER");
+		Vector<String> allSongs = r.getAllSongs();
+		for(int i = 0; i < allSongs.size(); i++) {
+			output += allSongs.get(i) + ",";
 		}
-	}
-
-
-
-	public String getUpdate() throws ClassNotFoundException, IOException {
-		// TODO Auto-generated method stub
-		Vector<String> input = new Vector<String>();
-		String username = "";
-		input.add(username);
-		Message m = new Message("UPDATE_SONG", input);
-		this.sendMessage(m);
-		Message mr = (Message)ois.readObject();
-		return mr.getInput().get(0);
-	}
-
-
-
-	public String getNum(String username) throws ClassNotFoundException, IOException {
-		Vector<String> input = new Vector<String>();
-		input.add(username);
-		System.out.println("USER NAME IN CLIENT: " + username);
-		Message m = new Message("GET_NUM", input);
-		this.sendMessage(m);
-		
-		Message mr = (Message)ois.readObject();
-		System.out.println("IN client " + mr.getInput().get(0));
-		return mr.getInput().get(0);
+		System.out.println("upVote: " + output);
+		Vector<String> outputSongs = new Vector<String>();
+		outputSongs.add(output);
+		Message m = new Message("output", outputSongs);
+		st.sendMessage(m);
 	}
 	
-	public String getNumF(String username) throws ClassNotFoundException, IOException {
-		Vector<String> input = new Vector<String>();
-		input.add(username);
-		System.out.println("USER NAME IN CLIENT: " + username);
-		Message m = new Message("GET_NUMF", input);
-		this.sendMessage(m);
-		
-		Message mr = (Message)ois.readObject();
-		System.out.println("IN client NUMF " + mr.getInput().get(0));
-		return mr.getInput().get(0);
+	public void upVoteF(Vector<String>input, ServerThread st) {
+		String output = "";
+		String songName = input.get(0);
+		MusicRoom r = musicroom.get(0);
+		r.upVoteF(songName);
+		Vector<String> allSongs = r.getAllSongs();
+		for(int i = 0; i < allSongs.size(); i++) {
+			output += allSongs.get(i) + ",";
+		}
+		System.out.println("upVoteF: " + output);
+		Vector<String> outputSongs = new Vector<String>();
+		outputSongs.add(output);
+		Message m = new Message("output", outputSongs);
+		st.sendMessage(m);
+	}
+	
+	
+	public void updateSong(Vector<String> vector, ServerThread st) {
+		if(musicroom.size() == 0) {
+			MusicRoom n = new MusicRoom("U45XYgJk");
+			musicroom.add(n);
+		}
+		MusicRoom r = musicroom.get(0);
+		String output = "";
+		System.out.println("UPDATE SONG SERVER:");
+		Vector<String> allSongs = r.getAllSongs();
+		for(int i = 0; i < allSongs.size(); i++) {
+			output += allSongs.get(i) + ",";
+		}
+		System.out.println("server song " + output);
+		Vector<String> outputSongs = new Vector<String>();
+		outputSongs.add(output);
+		Message m = new Message("output", outputSongs);
+		st.sendMessage(m);
+	}
+	
+	public void getNum(Vector<String>input, ServerThread st) {
+		String output = ""; 
+		MusicRoom r = musicroom.get(0);
+		String username = input.get(0);
+		output = r.getNum();
+		Vector<String> outputSongs = new Vector<String>();
+		outputSongs.add(output);
+		//System.out.println(username);
+		//System.out.println("NUM : " + output);
+		Message m = new Message("output", outputSongs);
+		st.sendMessage(m);
+	}
+	
+	public void getNumF(Vector<String>input, ServerThread st) {
+		String output = ""; 
+		MusicRoom r = musicroom.get(0);
+		String username = input.get(0);
+		output = r.getNum();
+		Vector<String> outputSongs = new Vector<String>();
+		outputSongs.add(output);
+		//System.out.println(username);
+		//System.out.println("NUM : " + output);
+		Message m = new Message("output", outputSongs);
+		st.sendMessage(m);
+	}
+	
+	public static void main(String [] args) {
+		Server cr = new Server();
 	}
 }
